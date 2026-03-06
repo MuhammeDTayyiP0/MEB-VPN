3ffc42efe77d9d25e9f3bbbd7ce8298e8e44906b
MuhammeDTayyiP0 <mtedeilim0@gmail.com>
2026-03-06 20:19:44 +0300

    feat: v1.3.0 universal proxy support and ironclad icons

--- src/main/proxy-settings.js
+++ src/main/proxy-settings.js
@@ -8,37 +8,42 @@ class ProxySettings {
     }
 
     async enable(host, port) {
-        const proxyAddress = `${host}:${port + 1}`; // HTTP proxy port (10809)
-        const socksAddress = `socks=${host}:${port}`;
+        const httpPort = port + 1;
+        const proxyAddress = `${host}:${httpPort}`;
 
-        console.log(`Enabling system proxy: ${proxyAddress}`);
+        console.log(`Enabling system proxy: HTTP=${proxyAddress}, SOCKS=${host}:${port}`);
 
         try {
             if (this.platform === 'win32') {
-                // Windows: Set global proxy and also per-protocol for better coverage
-                await this._execCommand(
-                    `reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f`
-                );
-                await this._execCommand(
-                    `reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyServer /t REG_SZ /d "${proxyAddress}" /f`
-                );
-                // Force specific protocols just in case
-                await this._execCommand(
-                    `reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyOverride /t REG_SZ /d "localhost;127.*;<local>" /f`
-                );
+                await this._execCommand(`reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f`);
+                await this._execCommand(`reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyServer /t REG_SZ /d "${proxyAddress}" /f`);
+                await this._execCommand(`reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyOverride /t REG_SZ /d "localhost;127.*;<local>" /f`);
             } else if (this.platform === 'linux') {
+                // Try GNOME (gsettings)
                 try {
-                    // GNOME / Pardus standard
                     await this._execCommand(`gsettings set org.gnome.system.proxy mode 'manual'`);
                     await this._execCommand(`gsettings set org.gnome.system.proxy.http host '${host}'`);
-                    await this._execCommand(`gsettings set org.gnome.system.proxy.http port ${port + 1}`);
+                    await this._execCommand(`gsettings set org.gnome.system.proxy.http port ${httpPort}`);
                     await this._execCommand(`gsettings set org.gnome.system.proxy.https host '${host}'`);
-                    await this._execCommand(`gsettings set org.gnome.system.proxy.https port ${port + 1}`);
+                    await this._execCommand(`gsettings set org.gnome.system.proxy.https port ${httpPort}`);
                     await this._execCommand(`gsettings set org.gnome.system.proxy.socks host '${host}'`);
                     await this._execCommand(`gsettings set org.gnome.system.proxy.socks port ${port}`);
-                    console.log('GNOME proxy settings applied via gsettings');
+                    console.log('GNOME proxy settings applied');
                 } catch (e) {
-                    console.log('gsettings failed, user might not be on GNOME:', e.message);
+                    console.log('GNOME gsettings failed:', e.message);
+                }
+
+                // Try KDE (kwriteconfig5)
+                try {
+                    await this._execCommand(`kwriteconfig5 --file kioslaverc --group "Proxy Settings" --key "ProxyType" 1`);
+                    await this._execCommand(`kwriteconfig5 --file kioslaverc --group "Proxy Settings" --key "httpProxy" "http://${host} ${httpPort}"`);
+                    await this._execCommand(`kwriteconfig5 --file kioslaverc --group "Proxy Settings" --key "httpsProxy" "http://${host} ${httpPort}"`);
+                    await this._execCommand(`kwriteconfig5 --file kioslaverc --group "Proxy Settings" --key "socksProxy" "socks://${host} ${port}"`);
+                    // Notify KDE about the change
+                    await this._execCommand(`dbus-send --type=signal /KIO/Scheduler org.kde.KIO.Scheduler.reparseSlaveConfiguration string:''`);
+                    console.log('KDE proxy settings applied');
+                } catch (e) {
+                    console.log('KDE kwriteconfig5 failed:', e.message);
                 }
             }
             this.enabled = true;
@@ -52,15 +57,16 @@ class ProxySettings {
         console.log('Disabling system proxy');
         try {
             if (this.platform === 'win32') {
-                await this._execCommand(
-                    `reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f`
-                );
+                await this._execCommand(`reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f`);
             } else if (this.platform === 'linux') {
+                // Disable GNOME
+                try { await this._execCommand(`gsettings set org.gnome.system.proxy mode 'none'`); } catch (e) { }
+
+                // Disable KDE
                 try {
-                    await this._execCommand(`gsettings set org.gnome.system.proxy mode 'none'`);
-                } catch (e) {
-                    console.error('gsettings disable failed:', e.message);
-                }
+                    await this._execCommand(`kwriteconfig5 --file kioslaverc --group "Proxy Settings" --key "ProxyType" 0`);
+                    await this._execCommand(`dbus-send --type=signal /KIO/Scheduler org.kde.KIO.Scheduler.reparseSlaveConfiguration string:''`);
+                } catch (e) { }
             }
             this.enabled = false;
         } catch (error) {
